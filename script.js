354d67b0635a4f84aceacec1fb16d88f95a98e2d
V-Frost <mv.vinchi@gmail.com>
2026-04-06 13:43:30 +0300

    Позначеним завдання "виконано"

--- script.js
+++ script.js
@@ -3,27 +3,62 @@ const addTaskButton = document.getElementById('addTaskButton');
 const taskList = document.getElementById('taskList');
 
 
+
+
 function addTask() {        
-    if (!taskInput.value.trim() ) {
+    if (!taskInput.value.trim()) {
         alert('Будь ласка, введіть завдання');
         return;
     }
     
     const taskItem = document.createElement('li');
 
-        const deleteButton = document.createElement('button');
-        deleteButton.textContent = '❌';
-        deleteButton.classList.add('deleteButton');
 
-        taskItem.textContent = taskInput.value;
-        taskItem.appendChild(deleteButton);
+    const span = document.createElement('span');
+    span.textContent = taskInput.value;
+    span.classList.add('task-text');
+    
+    const buttonContainer = document.createElement('div');
+    buttonContainer.classList.add('buttonManage');
+    
+    const completeBtn = document.createElement('button');
+    completeBtn.textContent = '✅';
+    completeBtn.classList.add('completeButton');
 
-        deleteButton.addEventListener('click', function() {
-            taskItem.remove();
-        });
+    const deleteBtn = document.createElement('button');
+    deleteBtn.textContent = '❌';
+    deleteBtn.classList.add('deleteButton');
 
-        taskList.appendChild(taskItem);
-        taskInput.value = '';
+    buttonContainer.appendChild(completeBtn);
+    buttonContainer.appendChild(deleteBtn);
+
+    taskItem.appendChild(span);
+    taskItem.appendChild(buttonContainer);
+
+    taskList.appendChild(taskItem);
+
+    deleteBtn.addEventListener('click', function() {
+        taskItem.remove();
+    })
+
+    taskInput.value = '';
+    let isCompleted = false; 
+    
+    completeBtn.addEventListener('click', function() {
+        if (!isCompleted) {
+
+            completeBtn.textContent = '↩️';
+            span.classList.add('completed');
+            isCompleted = true;
+
+        } else {
+
+            completeBtn.textContent = '✅';
+            span.classList.remove('completed');
+            isCompleted = false;
+          
+        }
+    })
 }
 
 addTaskButton.addEventListener('click', addTask);
